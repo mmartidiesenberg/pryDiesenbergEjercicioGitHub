9c589caa1ce3d9b623f6c9e6022510c05467703f
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-08-27 13:09:33 -0300

    agregue un boton

---
+++ pryDiesenbergEjercicio/frmLogin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace pryDiesenbergEjercicio
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUsuario = new Label();
+            lblContraseña = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(57, 48);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.TabIndex = 0;
+            lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            lblContraseña.AutoSize = true;
+            lblContraseña.Location = new Point(57, 111);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(67, 15);
+            lblContraseña.TabIndex = 1;
+            lblContraseña.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(143, 111);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(143, 45);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(304, 44);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.RightToLeft = RightToLeft.No;
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(304, 111);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.RightToLeft = RightToLeft.Yes;
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(486, 198);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(lblContraseña);
+            Controls.Add(lblUsuario);
+            Name = "frmLogin";
+            Text = "LOGIN";
+            Load += this.frmLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsuario;
+        private Label lblContraseña;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnAceptar;
+        private Button btnCancelar;
+    }
+}
